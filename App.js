7ca4e40cf91a9c3ef5f95406d57810a5c7dc782f
luthfiybk <luthfaryamg@gmail.com>
2022-11-30 16:41:13 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,117 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 
-export default function App() {
+import React, {useState} from "react";
+import { 
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  Text,
+  View,
+} from "react-native";
+
+const FormApp = () => {
+  const [text, onChangeText] = React.useState("")
+  const [text2, onChangeText2] = React.useState("")
+  const [text3, onChangeText3] = React.useState("")
+
+  const [result1, setValue1] = useState("")
+  const [result2, setValue2] = useState("")
+  const [result3, setValue3] = useState("")
+
+  const hello = () => {
+    setValue1(text)
+    setValue2(text2)
+    setValue3(text3)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView>
+      <TextInput
+        style={styles.inputNama}
+        onChangeText={onChangeText}
+        value={text}
+        placeholder="Nama"
+      />
+      <TextInput
+        style={styles.inputAsal}
+        onChangeText={onChangeText2}
+        value={text2}
+        placeholder="Asal"
+      />
+      <TextInput
+        style={styles.inputNim}
+        onChangeText={onChangeText3}
+        value={text3}
+        placeholder="NIM"
+      />
+      <TouchableOpacity
+        style={styles.buttonSubmit}
+        onPress={hello}
+      >
+        <Text style={styles.textSubmit}>Submit</Text>
+      </TouchableOpacity>
+
+      <View style={styles.viewResult}>
+        <Text style={styles.textResult}>Nama: {result1}</Text>
+        <Text style={styles.textResult}>Asal: {result2}</Text>
+        <Text style={styles.textResult}>NIM: {result3}</Text>
+      </View>
+    </SafeAreaView>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const styles = StyleSheet.create ({
+  inputNama:{
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
-});
+
+  inputAsal: {
+    height: 40,
+    marginBottom: 12,
+    marginLeft: 12,
+    marginRight: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+
+  inputNim: {
+    height: 40, 
+    marginBottom: 10,
+    marginLeft:12,
+    marginRight: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+
+  buttonSubmit: {
+    height: 40,
+    alignItems: "center",
+    backgroundColor: "#0000FF",
+    color: "#000000",
+    marginLeft: 12,
+    marginRight: 12,
+    padding: 10,
+  },
+
+  textSubmit: {
+    color: "#FFFFFF"
+  },
+
+  viewResult: {
+    marginTop: 20,
+    marginLeft: 12,
+    marginRight: 12,
+  },
+})
+
+export default FormApp
